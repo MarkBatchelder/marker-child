5b1c6f7e628c5812ea947752f9e3bc650eb052d2
Mark Batchelder <code@wpaddiction.com>
2015-11-03 03:17:52 -0800

    Added missing period in a load_theme_textdomain() comment.

--- functions.php
+++ functions.php
@@ -33,7 +33,7 @@ function marker_setup() {
 	 * Make theme available for translation.
 	 * Translations can be filed in the /languages/ directory.
 	 * If you're building a theme based on Marker, use a find and replace
-	 * to change 'marker' to the name of your theme in all the template files
+	 * to change 'marker' to the name of your theme in all the template files.
 	 */
 	load_theme_textdomain( 'marker', get_template_directory() . '/languages' );
 
