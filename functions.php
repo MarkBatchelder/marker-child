820940c5fe57acc61536327237c77a1fd75d7b41
Mark Batchelder <code@wpaddiction.com>
2015-12-16 01:28:38 -0800

    Fixed up child theme enqueue.
    https://codex.wordpress.org/Child_Themes

--- functions.php
+++ functions.php
@@ -9,16 +9,19 @@
 
 /**
  * This enqueues the parent and child theme stylesheets.
- * http://codex.wordpress.org/Child_Themes
+ * https://codex.wordpress.org/Child_Themes
  */
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
-	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+	$parent_style = 'parent-style';
+
+	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'child-style',
 		get_stylesheet_directory_uri() . '/style.css',
-		array('parent-style')
+		array( $parent_style )
 	);
 }
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
 if ( ! function_exists( 'marker_setup' ) ) :
 /**
