ac7ebddfaf7c8b995fb9300cf50e20c52c87d8c4
WPAddiction <code@wpaddiction.com>
2025-01-01 14:43:07 -0800

    Merge pull request #1140 from truongwp/patch-1
    Correct code indentation to align with WordPress code standards

--- functions.php
+++ functions.php
@@ -24,92 +24,79 @@ function theme_enqueue_styles() {
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
 if ( ! function_exists( 'marker_setup' ) ) :
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- */
-function marker_setup() {
-	/*
-	 * Make theme available for translation.
-	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on Marker, use a find and replace
-	 * to change 'marker' to the name of your theme in all the template files.
-	 */
-	load_theme_textdomain( 'marker', get_template_directory() . '/languages' );
-
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
-
-	/*
-	 * Let WordPress manage the document title.
-	 * By adding theme support, we declare that this theme does not use a
-	 * hard-coded <title> tag in the document head, and expect WordPress to
-	 * provide it for us.
-	 */
-	add_theme_support( 'title-tag' );
-
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
-	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
 	 */
-	add_theme_support( 'post-thumbnails' );
+	function marker_setup() {
+		/*
+		 * Make theme available for translation.
+		 * Translations can be filed in the /languages/ directory.
+		 * If you're building a theme based on Marker, use a find and replace
+		 * to change 'marker' to the name of your theme in all the template files.
+		 */
+		load_theme_textdomain( 'marker', get_template_directory() . '/languages' );
 
-	// This theme uses wp_nav_menu() in two locations.
-	register_nav_menus( array(
-		'top'     => esc_html__( 'Top Navigation', 'marker' ),
-		'primary' => esc_html__( 'Primary Navigation', 'marker' ),
-	) );
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
 
-	/*
-	 * Switch default core markup for search form, comment form, and comments
-	 * to output valid HTML5.
-	 */
-	add_theme_support( 'html5', array(
-		'search-form',
-		'comment-form',
-		'comment-list',
-		'gallery',
-		'caption',
-	) );
+		/*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support( 'title-tag' );
 
-	/*
-	 * Enable support for Post Formats.
-	 * See https://developer.wordpress.org/themes/functionality/post-formats/
-	 *
-	add_theme_support( 'post-formats', array(
-		'aside',
-		'image',
-		'video',
-		'quote',
-		'link',
-	) );
-	 */
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+		add_theme_support( 'post-thumbnails' );
 
-	// Set up the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'marker_custom_background_args', array(
-		'default-color'	=> 'ffffff',
-		'default-image'	=> '',
-	) ) );
+		// This theme uses wp_nav_menu() in two locations.
+		register_nav_menus( array(
+			'top'     => esc_html__( 'Top Navigation', 'marker' ),
+			'primary' => esc_html__( 'Primary Navigation', 'marker' ),
+		) );
 
-    // Add theme support for selective refresh for widgets.
-    add_theme_support( 'customize-selective-refresh-widgets' );
+		/*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+		add_theme_support( 'html5', array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+		) );
 
-    /**
-     * Add support for core custom logo.
-     *
-     * @link https://codex.wordpress.org/Theme_Logo
-     */
-    add_theme_support( 'custom-logo', array(
-        'height'      => 250,
-        'width'       => 250,
-        'flex-width'  => true,
-        'flex-height' => true,
-    ) );
-}
+		// Set up the WordPress core custom background feature.
+		add_theme_support( 'custom-background', apply_filters( 'marker_custom_background_args', array(
+			'default-color'	=> 'ffffff',
+			'default-image'	=> '',
+		) ) );
+
+		// Add theme support for selective refresh for widgets.
+		add_theme_support( 'customize-selective-refresh-widgets' );
+
+		/**
+		 * Add support for core custom logo.
+		 *
+		 * @link https://codex.wordpress.org/Theme_Logo
+		 */
+		add_theme_support( 'custom-logo', array(
+			'height'      => 250,
+			'width'       => 250,
+			'flex-width'  => true,
+			'flex-height' => true,
+		) );
+	}
 endif;
 add_action( 'after_setup_theme', 'marker_setup' );
 
