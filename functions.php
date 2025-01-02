0f8c6880b257a10ac4744b9c6ab54101e6743561
WPAddiction <code@wpaddiction.com>
2025-01-02 13:20:22 -0800

    Merge pull request #1267 from jrfnl/feature/cs-update
    Bring code style up to the latest standards
    - This PR addresses code-style issues identified when testing the code against the WordPress Coding Standards.

--- functions.php
+++ functions.php
@@ -108,6 +108,9 @@ add_action( 'after_setup_theme', 'marker_setup' );
  * @global int $content_width
  */
 function marker_content_width() {
+    // This variable is intended to be overruled from themes.
+    // Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
+    // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
 	$GLOBALS['content_width'] = apply_filters( 'marker_content_width', 730 );
 }
 add_action( 'after_setup_theme', 'marker_content_width', 0 );
